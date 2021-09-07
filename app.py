a19fe44129f76e8855d28b5b98ce251087bc1ac7
Thatchapatt (Tar) <74225067+utpk@users.noreply.github.com>
2021-09-07 11:41:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ while True:
   time.sleep(10) # pause for 10 seconds
   try:
     url = endpoints[i%3]
-    print("curl ",url,"... ",end='', flush=True)
-    r = requests.head(url, timeout=30, verify=Fause)
+    print("curl",url,"... ",end='', flush=True)
+    r = requests.head(url, timeout=30, verify=False)
     r.raise_for_status()
   except Exception as e:
-    print("FAILED",time.localtime())
+    print("FAILED",time.ctime())
   else:
     print("OK")
   finally:
