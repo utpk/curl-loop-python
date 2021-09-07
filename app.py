dd452303612ed0dcf98220898778d94c3bf995c9
Thatchapatt (Tar) <74225067+utpk@users.noreply.github.com>
2021-09-07 12:29:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import requests
 import time
+import urllib3
+
+urllib3.disable_warnings()
 
 print("Startup...")
-endpoints = ["https://login.microsoftonline.com","https://github.com","https://welltesting.free.beeceptor.com"]
-i=0
+endpoints = ["https://login.microsoftonline.com", "https://github.com"]
+i = 0
+fail_count = 0
 
 while True:
-  time.sleep(10) # pause for 10 seconds
-  try:
-    url = endpoints[i%3]
-    print("curl",url,"... ",end='', flush=True)
-    r = requests.head(url, timeout=30, verify=False)
-    r.raise_for_status()
-  except Exception as e:
-    print("FAILED",time.ctime())
-  else:
-    print("OK")
-  finally:
-    i+=1
+    time.sleep(10)
+    try:
+        url = endpoints[i % len(endpoints)]
+        print("curl", url, "... ", end="", flush=True)
+        r = requests.head(url, timeout=30, verify=False)
+        r.raise_for_status()
+    except Exception as e:
+        fail_count += 1
+        print(r.elapsed, "FAILED", fail_count, time.ctime())
+    else:
+        print(r.elapsed, "OK")
+    finally:
+        i += 1
